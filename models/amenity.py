063c653ea36524b669ce5cbb235ab195a7435062
Piperkfb <piperkfb@gmail.com>
2023-10-31 13:50:40 -0500

    Amenities

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 class Amenity(BaseModel):
-    name = ""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
